9c0a088ae28723b096aae5a1d7b80087c9242e68
Bruno Cicciarello <bruno.cicciarello@gmail.com>
2024-06-23 20:05:13 +0100

    Allowing mutability in Migration and tests

--- src/storage/Migration.ts
+++ src/storage/Migration.ts
@@ -11,11 +11,24 @@ export const migrateState = async (
   loadedVersion: number | undefined
 ) => {
   Log.d?.(`Migrating from version ${loadedVersion} to ${config.version} ...`);
-  const migratedState = await config.migrateState(
-    loadedState,
+  const tempState = await config.migrateState(
+    { ...loadedState },
     loadedVersion,
     config.version
   );
+
+  // Detect changes in the new state, so that the upgrade routine doesn't have
+  // to worry about immutability
+  const migratedState: KeyValueStore = {};
+  for (const key in tempState) {
+    if (!deepEqual(loadedState[key], tempState[key])) {
+      // This slice has been changed: use the new one
+      migratedState[key] = tempState[key];
+    } else {
+      // This slice has not been changed: reuse the one we loaded
+      migratedState[key] = loadedState[key];
+    }
+  }
 
   // Save the newly updated state and its version
   Log.d?.("Saving migrated state...");
@@ -24,3 +37,35 @@ export const migrateState = async (
   Log.d?.("Migration complete.");
   dispatch(readyAction(migratedState, config.version));
 };
+
+function isObject(obj: any): boolean {
+  return obj !== null && typeof obj === "object";
+}
+
+function deepEqual(obj1: any, obj2: any): boolean {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (!isObject(obj1) || !isObject(obj2)) {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (!keys2.includes(key)) {
+      return false;
+    }
+    if (!deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
--- src/storage/__tests__/Migrate.test.ts
+++ src/storage/__tests__/Migrate.test.ts
@@ -1,5 +1,5 @@
 import { Reducer } from "redux";
-import { Config, sanitizeConfig } from "../../config";
+import { Config, KeyValueStore, sanitizeConfig } from "../../config";
 import { readyAction } from "../../state";
 import { NullStorage } from "../Storage";
 import { migrateState } from "../Migration";
@@ -18,7 +18,7 @@ describe("Migrate", () => {
       item_b: { text: "World" },
     };
     const mockMigrate = jest.fn().mockReturnValue(mockMigratedState);
-    const mockSaveState = jest.spyOn(SaverModule, "saveState");
+    const mockSaveState = jest.spyOn(SaverModule, "saveState").mockReset();
     const config: Config = sanitizeConfig({
       reducer: {
         item1: {} as Reducer,
@@ -46,5 +46,48 @@ describe("Migrate", () => {
     expect(mockDispatch).toHaveBeenCalledWith(
       readyAction(mockMigratedState, 43)
     );
+  });
+
+  it("should allow mutability during state migration", async () => {
+    const mockLoadedState = {
+      item1: { value: 1 },
+      item2: { value: 2 },
+      item3: { value: 3 },
+    };
+    const mutatingMigrate = async (
+      state: KeyValueStore
+    ): Promise<KeyValueStore> => {
+      // Mutates the received state by adding a new property
+      state.item1.text = "Ciao";
+      delete state.item3;
+      return state;
+    };
+    const mockSaveState = jest.spyOn(SaverModule, "saveState").mockReset();
+    const config: Config = sanitizeConfig({
+      reducer: {
+        item1: {} as Reducer,
+        item2: {} as Reducer,
+        item3: {} as Reducer,
+      },
+      storage: NullStorage,
+      migrateState: mutatingMigrate,
+      version: 43,
+    });
+    const mockDispatch = jest.fn();
+
+    await migrateState(mockDispatch, config, mockLoadedState, 42);
+
+    console.log(JSON.stringify(mockSaveState.mock.calls[0][1]));
+
+    expect(mockSaveState.mock.calls[0][0]).toBe(mockLoadedState);
+    // Item1 should have been updated
+    expect(mockSaveState.mock.calls[0][1]["item1"]).toEqual({
+      value: 1,
+      text: "Ciao",
+    });
+    // Item2 should be unchanged
+    expect(mockSaveState.mock.calls[0][1]["item2"]).toBe(mockLoadedState.item2);
+    // Item3 should be no longer there
+    expect(mockSaveState.mock.calls[0][1]["item3"]).toBeUndefined();
   });
 });
